51a2da01bd593f05f4956da985796c5d6fa70827
Kamil Ruchaj <kamilruchaj@gmail.com>
2021-12-06 00:05:40 +0100

    Initial file

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 from flask import Flask, jsonify, render_template, request
 from flask_sqlalchemy import SQLAlchemy
+import random
+import pprint
 
 app = Flask(__name__)
 
@@ -27,7 +29,35 @@ class Cafe(db.Model):
 @app.route("/")
 def home():
     return render_template("index.html")
-    
+
+
+@app.route("/random")
+def get_random_cafe():
+    data = Cafe.query.all()
+    random_data = random.choice(data)
+
+    return jsonify(data=random_data.to)
+
+
+        jsonify(data=jsonify(
+        id=random_data.id,
+        map_url=random_data.map_url,
+        img_url=random_data.img_url,
+        location=random_data.location,
+        seats=random_data.seats,
+        has_toilet=random_data.has_toilet,
+        has_wifi=random_data.has_wifi,
+        has_sockets=random_data.has_sockets,
+        can_take_calls=random_data.can_take_calls,
+        coffee_price=random_data.coffee_price
+    ).json)
+
+    # random_cafe = movie_to_update = Cafe.query.get(movie_id)
+
+    # print(random_cafe)
+
+    # Movie.query.order_by(Movie.rating.desc()).all()
+
 
 ## HTTP GET - Read Record
 
